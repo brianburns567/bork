388b25fe922bf06d9722639e9254d7387ef55626
andrewjwoodruff <awoodruf@mail.umw.edu>
2016-11-09 19:29:06 -0500

    Create HealthCommand.java
    w/ skeleton implementation and JavaDoc comments

---
+++ HealthCommand.java
@@ -0,0 +1,17 @@
+
+package bork;
+
+/**
+ * Handles the command to view the user's current health.
+ * @author Woodruff
+ */
+class HealthCommand extends Command {
+
+      HealthCommand() {}
+      
+      /**
+       * Returns the String based on the players current health
+       * @return String the line expressing how healthy the player is
+       */
+      public String execute() {}
+}
