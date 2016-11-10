c961b9482f2f09c223728f9ddef99e8be28edb0e
andrewjwoodruff <awoodruf@mail.umw.edu>
2016-11-09 19:35:56 -0500

    Create AttackCommand.java
    w/ skeleton implementation and JavaDoc Comments

---
+++ AttackCommand.java
@@ -0,0 +1,25 @@
+
+package bork;
+
+/**
+ * Handles the command to attack something/one else
+ * @author Woodruff
+ */
+class AttackCommand extends Command {
+
+      private Weapon weapon;
+      private NPC target;
+      
+      /**
+       * Constructor for the weapon the player uses and the targer of the attack
+       * @param weaponName the name of the weapon the player is attacking with
+       * @param targerName the name of the target the player is attacking
+       */
+      AttackCommand(String weaponName, String targetName) {}
+      
+      /**
+       * Returns the String based on the calculated combat exchange 
+       * @return String with lines of combat and messages about the round
+       */
+      public String execute() {}
+}
