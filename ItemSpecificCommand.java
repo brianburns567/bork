da93a1f38e8d959ae3e7801cff8dad9c902ed328
andrewjwoodruff <awoodruf@mail.umw.edu>
2016-11-09 19:12:33 -0500

    Update ItemSpecificCommand.java

--- ItemSpecificCommand.java
+++ ItemSpecificCommand.java
@@ -1,17 +1,29 @@
 
 package bork;
 
+/**
+ * Handles commands for specific items
+ * @author Dr. Zeitz
+ */
 class ItemSpecificCommand extends Command {
 
     private String verb;
     private String noun;
-                        
-
+    
+    /**
+     * Constructor for the ItemSpecificCommand with the input of String verb and noun
+     * @param verb the verb refernced in the nouns messages
+     * @param noun the string name of the item being refernced
+     */
     ItemSpecificCommand(String verb, String noun) {
         this.verb = verb;
         this.noun = noun;
     }
 
+    /**
+     * Returns the String based on the item being referenced
+     * @return String the message of the item, the message that the item isn't here, or the message that you can't use that verb on this item
+     */
     public String execute() {
         
         Item itemReferredTo = null;
