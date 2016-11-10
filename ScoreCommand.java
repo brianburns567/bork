56971a5b334e3740afff6ea39fc318d3d71fec01
andrewjwoodruff <awoodruf@mail.umw.edu>
2016-11-09 19:39:29 -0500

    Create ScoreCommand.java
    w/ skeleton implementation and JavaDoc comments

---
+++ ScoreCommand.java
@@ -0,0 +1,17 @@
+
+package bork;
+
+/**
+ * Handles commands to view the players current score
+ * @author Woodruff
+ */
+class ScoreCommand extends Command {
+
+      ScoreCommand() {}
+      
+      /**
+       * Returns the String of the players current score with a message about their ranking/level
+       * @return String message with the score and ranking
+       */
+      public String execute() {}
+}
