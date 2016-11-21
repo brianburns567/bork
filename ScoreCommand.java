9204de647283d0e5205d97620b041f334365da03
brianburns567 <brianburns567@yahoo.com>
2016-11-21 16:42:52 -0500

    ScoreCommand written

--- ScoreCommand.java
+++ ScoreCommand.java
@@ -13,5 +13,24 @@ class ScoreCommand extends Command {
        * Returns the String of the players current score with a message about their ranking/level
        * @return String message with the score and ranking
        */
-      public String execute() {return "";}
+      public String execute() {
+          int score = GameState.instance().getScore();
+          String desc = "You have accumulated " + score + " points. Your current"
+                  + " rank is ";
+          
+          if (score < 25) {
+              desc += "Amateur Adventurer.\n";
+          }
+          else if (score < 50) {
+              desc += "Common Adventurer.\n";
+          }
+          else if (score < 75) {
+              desc += "Advanced Adventurer.\n";
+          }
+          else {
+              desc += "Expert Adventurer.\n";
+          }
+          
+          return desc;
+      }
 }
