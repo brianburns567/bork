a50c3aab64b65726c013568ed82d9000721bdad5
brianburns567 <brianburns567@yahoo.com>
2016-11-21 16:31:21 -0500

    HealthCommand implementation

--- HealthCommand.java
+++ HealthCommand.java
@@ -13,5 +13,21 @@ class HealthCommand extends Command {
        * Returns the String based on the players current health
        * @return String the line expressing how healthy the player is
        */
-      public String execute() {return "";}
+      public String execute() {
+          int health = GameState.instance().getHealth();
+          
+          if (health > 74) {
+              return "You are at your peak performance.\n";
+          }
+          else if (health > 49) {
+              return "You're a bit fatigued.\n";
+          }
+          else if (health > 24) {
+              return "You're pretty banged up.\n";
+          }
+          else {
+              return "Think Bruce Willis near the end of \"Die Hard\". Each "
+                      + "move from here on out could be your last.\n";
+          }
+      }
 }
